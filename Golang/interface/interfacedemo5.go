2a8cb655de9cbf6ddbd8643b2e658607bf65d7eb
Cris <brandt84828@gmail.com>
2022-12-05 20:40:56 +0800

    update interface demo5

--- Golang/interface/interfacedemo5.go
+++ Golang/interface/interfacedemo5.go
@@ -15,7 +15,8 @@ func (this *Stu) Say() {
 
 func main() {
 	var stu Stu = Stu{}
-	var u Usb = stu //錯誤，因為這邊是用stu類型，但定義的Say方法用的*Stu則是指針類型
+	var u Usb = stu //錯誤，因為這邊是用stu類型，但定義的Say方法用的*Stu則是指針類型，Stu類型沒有實現Usb interface
+	//正確的要改 var u Usb = &stu
 	u.Say()
 	fmt.Println("Here", u)
 }
