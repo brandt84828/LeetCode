f085d21ed2fed61932958655e1ce1648f4f602be
Cris <brandt84828@gmail.com>
2022-12-13 22:12:22 +0800

    assert demo 2

--- Golang/assert/assertdemo2.go
+++ Golang/assert/assertdemo2.go
@@ -8,7 +8,8 @@ func main() {
 	x = b //empty interface can accept any type
 
 	//這樣寫不會因為類型轉換失敗而直接結束程式
-	if y, ok := x.(float32); ok {
+	y, ok := x.(float32)
+	if ok {
 		fmt.Printf("y的類型是%T,值是%v", y, y)
 	} else {
 		fmt.Println("convert fail")
