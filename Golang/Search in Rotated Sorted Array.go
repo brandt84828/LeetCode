e1773fbabc2b625b8d01d7ab699dd0659653a935
Cris <brandt84828@gmail.com>
2023-02-03 22:24:44 +0800

    Search in Rotated Sorted Array

---
+++ Golang/Search in Rotated Sorted Array.go
@@ -0,0 +1,23 @@
+func search(nums []int, target int) int {
+    low := 0
+    high := len(nums) - 1
+    for low <= high {
+        mid := (low + high) / 2
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] >= nums[low] {
+            if target >= nums[low] && nums[mid] > target {
+                high = mid - 1
+            } else {
+                low = mid + 1
+            }
+        } else {
+            if target > nums[mid] && target <= nums[high] {
+                low = mid + 1
+            } else {
+                high = mid - 1
+            }
+        }
+    }
+    return -1
+}
