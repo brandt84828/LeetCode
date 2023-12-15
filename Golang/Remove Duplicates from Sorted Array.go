3271f6de566874e17e0354ea2573a776e1b7f710
Cris Hsu (許哲維) <10905902@qcttw.com>
2023-12-15 14:21:58 +0800

    26

--- Golang/Remove Duplicates from Sorted Array.go
+++ Golang/Remove Duplicates from Sorted Array.go
@@ -1,4 +1,27 @@
 func removeDuplicates(nums []int) int {
+    count := 1
+    for i :=1; i < len(nums);i ++{
+        if nums[i] > nums[count-1] {            
+            nums[count] = nums[i]
+            count = count + 1
+        }
+    }
+    return count
+}
+
+
+func removeDuplicates(nums []int) int {
+    j := 1
+    for i:=1; i < len(nums); i++ {
+        if nums[i] != nums[i-1] {
+            nums[j] = nums[i]
+            j++
+        }
+    }
+    return j
+}
+
+func removeDuplicates(nums []int) int {
     
     insert := 1
     for i:=1;i < len(nums);i++ {
--- Python3/Remove Duplicates from Sorted Array.py
+++ Python3/Remove Duplicates from Sorted Array.py
@@ -1,9 +1,10 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-
-        insert = 1
+        j = 1
         for i in range(1, len(nums)):
             if nums[i] != nums[i-1]:
-                nums[insert] = nums[i]
-                insert = insert + 1
-        return insert
+                nums[j] = nums[i]
+                j = j + 1
+
+        return j
+              
