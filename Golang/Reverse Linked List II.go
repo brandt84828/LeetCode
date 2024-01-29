f9e9f69c2f7a4af20f00e63461ac0f544f27f771
brandt84828 <brandt84828@gmail.com>
2024-01-29 08:48:31 +0800

    92

---
+++ Golang/Reverse Linked List II.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseBetween(head *ListNode, left int, right int) *ListNode {
+    if head == nil || left == right {
+        return head
+    }
+    
+    dummy := &ListNode{0, head}
+    prev := dummy
+    
+    for i := 0; i < left - 1; i++ {
+        prev = prev.Next
+    }
+    
+    current := prev.Next
+    
+    for i := 0; i < right - left; i++ {
+        nextNode := current.Next
+        current.Next = nextNode.Next
+        nextNode.Next = prev.Next
+        prev.Next = nextNode
+    }
+    
+    return dummy.Next
+}
