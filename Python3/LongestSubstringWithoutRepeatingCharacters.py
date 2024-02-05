6dd95d93c592ecabe5d876b00722e6b1d9705d91
brandt84828 <brandt84828@gmail.com>
2024-02-05 09:08:33 +0800

    3

--- Python3/LongestSubstringWithoutRepeatingCharacters.py
+++ Python3/LongestSubstringWithoutRepeatingCharacters.py
@@ -14,3 +14,20 @@
             length = max(length,r - l + 1)
 
         return length
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+
+        l = 0
+        length = 0
+        hashset = {}
+        for r in range(len(s)):
+
+            if s[r] in hashset:
+                l = max(hashset[s[r]]+1, l)
+                
+            hashset[s[r]] = r
+            length = max(length,r - l + 1)
+           # hashset[s[r]] = hashset[s[r]] + 1
+
+        return length
