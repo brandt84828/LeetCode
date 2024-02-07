42cd0c328ca9ed6cb5dfd3fcf6ec34b21149e90e
brandt84828 <brandt84828@gmail.com>
2024-02-07 08:41:27 +0800

    53

--- Python3/Maximum Subarray.py
+++ Python3/Maximum Subarray.py
@@ -1,3 +1,4 @@
+#1
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         res = nums[0]
@@ -8,3 +9,39 @@ class Solution:
             if total < 0:
                 total = 0
         return res
+
+#2
+class Solution:
+    def maxSubArray(self, nums):
+        dp = [[0]*len(nums) for i in range(2)]
+        dp[0][0], dp[1][0] = nums[0], nums[0]
+        for i in range(1, len(nums)):
+            dp[1][i] = max(nums[i], nums[i] + dp[1][i-1])
+            dp[0][i] = max(dp[0][i-1], dp[1][i])
+        return dp[0][-1]
+
+#3
+class Solution:
+    def maxSubArray(self, nums):
+        def maxSubArray(A, L, R):
+            if L > R: return -inf
+            mid, left_sum, right_sum, cur_sum = (L + R) // 2, 0, 0, 0
+            for i in range(mid-1, L-1, -1):
+                left_sum = max(left_sum, cur_sum := cur_sum + A[i])
+            cur_sum = 0
+            for i in range(mid+1, R+1):
+                right_sum = max(right_sum, cur_sum := cur_sum + A[i])
+            return max(maxSubArray(A, L, mid-1), maxSubArray(A, mid+1, R), left_sum + A[mid] + right_sum)
+        return maxSubArray(nums, 0, len(nums)-1)
+
+#4
+class Solution:
+    def maxSubArray(self, nums):
+        pre, suf = [*nums], [*nums]
+        for i in range(1, len(nums)):       pre[i] += max(0, pre[i-1])
+        for i in range(len(nums)-2,-1,-1):  suf[i] += max(0, suf[i+1])
+        def maxSubArray(A, L, R):
+            if L == R: return A[L]
+            mid = (L + R) // 2
+            return max(maxSubArray(A, L, mid), maxSubArray(A, mid+1, R), pre[mid] + suf[mid+1])
+        return maxSubArray(nums, 0, len(nums)-1)
